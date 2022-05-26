91aebee2e95f7a340217aa186209e78b7d9d6b89
CapnOdin <chip-10@hotmail.com>
2022-05-26 11:02:38 +0200

    improved the search time of regions by utilising parallelization

--- mcsearch/region.py
+++ mcsearch/region.py
@@ -1,22 +1,135 @@
 import anvil
 
-from nbt import nbt
+from . import searchable, chunk, constants, errors
 
-from . import searchable, chunk, constants
+import pebble
+#import concurrent.futures
+import time, dill, platform
+#import signal, psutil, os
+
+#_DEFAULT_PROCESS_POOL = concurrent.futures.ProcessPoolExecutor()
+_DEFAULT_PROCESS_POOL = pebble.ProcessPool()
 
 REGION_SIZE_IN_CHUNKS = 32
 
 REGION_SIZE = REGION_SIZE_IN_CHUNKS * chunk.CHUNK_SIZE
 
-class Region(searchable.Searchable, anvil.Region):
+def searchChunks20XX1(path, i, function, verbose = 0):
+	r = Region.from_file(path)
+	function = dill.loads(function)
+	aChunk = None
+	for j in range(REGION_SIZE_IN_CHUNKS):
+		try:
+			aChunk = chunk.Chunk.from_region(r, i, j)
+		except (anvil.errors.ChunkNotFound, IndexError) as e:
+			if(verbose >= constants.VERBOSE_ERRORS):
+				print(e)
+			continue
+		try:
+			for res in function(aChunk):
+				yield res
+		except Exception as e:
+			if(verbose >= constants.VERBOSE_ERRORS):
+				print(e)
+			raise e
+
+def searchChunks20XX2(path, i, function, verbose = 0):
+	lst = []
+	try:
+		#print(f"Thread {i} start")
+		r = Region.from_file(path)
+		function = dill.loads(function)
+		aChunk = None
+		for j in range(REGION_SIZE_IN_CHUNKS):
+			try:
+				aChunk = r.chunk(i, j)
+			except (anvil.errors.ChunkNotFound, IndexError) as e:
+				if(verbose >= constants.VERBOSE_ERRORS):
+					print(e)
+				continue
+			lst.append([])
+			try:
+				for res in function(aChunk):
+					lst[-1].append(res)
+			except Exception as e:
+				if(verbose >= constants.VERBOSE_ERRORS):
+					print(e)
+				raise e
+		#print(f"Thread {i} done")
+	except Exception as e:
+		pass
+	return lst
+
+def forAllChunksThreaded20XX(path, function, verbose = 0):
+	#time1 = time.time()
+	function = dill.dumps(function)
+	threads = []
+	try:
+		for i in range(1, REGION_SIZE_IN_CHUNKS):
+			threads.append(_DEFAULT_PROCESS_POOL.schedule(searchChunks20XX2, (path, i, function), {"verbose": verbose}))
+		#print("Threads Generated")
+		yield from searchChunks20XX1(path, 0, function, verbose = verbose)
+		#print(f"Threads output")
+		for thr in threads:
+			try:
+				chunkRes = thr.result()
+				for res in chunkRes:
+					for res2 in res:
+						yield res2
+			except (TypeError, pebble.common.ProcessExpired) as e:
+				pass
+	except KeyboardInterrupt as e: 
+		i = 0
+		for thr in threads:
+			if(thr.cancel()):
+				print(f"Canceled {i}")
+			i += 1
+		raise e
+	except Exception as e:
+		if(verbose >= constants.VERBOSE_ERRORS):
+			print(e)
+		raise e
+	#print("Threads Done")
+	#time2 = time.time()
+	#print('{:s} function took {:.3f} ms'.format("__forAllChunksThreaded20XX", (time2-time1)*1000.0))
+
+
+class Region(searchable.Searchable, area.Area, anvil.Region):
+	def __init__(self, data: bytes, path = ""):
+		super().__init__(data)
+		self.path = path
+		self.x = self.data["xPos"].value * REGION_SIZE
+		self.z = self.data["zPos"].value * REGION_SIZE
+
+	@classmethod
+	def from_file(cls, path):
+		r = super(Region, cls).from_file(path)
+		r.path = path
+		return r
+
+	def chunkByIndex(self, i, j):
+		return chunk.Chunk.from_region(self, i, j)
+
+	def chunkByCoords(self, x, z):
+		return self.chunkByIndex(*self.__coordsToChunkIndex(x, z))
+
 	def search_nbt(self, tags, keys = None, verbose = 0):
-		yield from self.__forAllChunks(lambda c: c.search_nbt(tags, keys = keys, verbose = verbose), verbose = verbose)
+		if(platform.python_implementation() == "PyPy"):
+			yield from self.__forAllChunks(lambda c: c.search_nbt(tags, keys = keys, verbose = verbose), verbose = verbose)
+		else:
+			yield from forAllChunksThreaded20XX(self.path, lambda c: c.search_nbt(tags, keys = keys, verbose = verbose), verbose = verbose)
 
 	def search_blocks(self, id, verbose = 0):
-		yield from self.__forAllChunks(lambda c: c.search_blocks(id, verbose = verbose), verbose = verbose)
+		#yield from self.__forAllChunks(lambda c: c.search_blocks(id, verbose = verbose), verbose = verbose)
+		#yield from self.__forAllChunksThreaded100(lambda c: c.search_blocks(id, verbose = verbose), verbose = verbose)
+		yield from forAllChunksThreaded20XX(self.path, lambda c: c.search_blocks(id, verbose = verbose), verbose = verbose)
+		#yield from self.__forAllChunksThreaded4(id, lambda c: c.search_blocks(id, verbose = verbose), verbose = verbose)
 	
+	# 103557.920 ms
+
 	def __forAllChunks(self, funtion, verbose = 0):
 		aChunk = None
+		#time1 = time.time()
 		for i in range(REGION_SIZE_IN_CHUNKS):
 			for j in range(REGION_SIZE_IN_CHUNKS):
 				try:
@@ -32,9 +145,15 @@ class Region(searchable.Searchable, anvil.Region):
 					if(verbose >= constants.VERBOSE_ERRORS):
 						print(e)
 					raise e
+		#time2 = time.time()
+		#print('{:s} function took {:.3f} ms'.format("__forAllChunks", (time2-time1)*1000.0))
 	
+	def __coordsToChunkIndex(self, x, z):
+		cx = (x - self.x) // chunk.CHUNK_SIZE
+		cz = (z - self.z) // chunk.CHUNK_SIZE
+		if(cx < 0 or cx > REGION_SIZE_IN_CHUNKS or cz < 0 or cz > REGION_SIZE_IN_CHUNKS):
+			raise errors.CoordsNotInArea((cx, cz), self)
+		return (cx, cz)
+
 	def __str__(self):
-		offx = self.data["xPos"].value * REGION_SIZE
-		offz = self.data["zPos"].value * REGION_SIZE
-		return f"<{type(self).__name__} at ({offx}, {offz})>"
-	
+		return f"<{type(self).__name__} at ({self.x}, {self.z})>"
